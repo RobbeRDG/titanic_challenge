b35dd251e8cc5df0939bfe9a92cdd97d5448e9fa
RobbeRDG <robbedegroeve@gmail.com>
2022-09-06 15:50:46 +0000

    Some data exploration

--- attempts/neural_network/config.py
+++ attempts/neural_network/config.py
@@ -1,12 +1,12 @@
 import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/clean/attempt_2_train.csv"
-VAL_DIR = "data/clean/attempt_2_test.csv"
+TRAIN_DIR = "data/clean/neural_network_train.csv"
+VAL_DIR = "data/clean/neural_network_test.csv"
 LEARNING_RATE = 5e-2
 BATCH_SIZE = 892
 NUM_WORKERS = 2
 LOAD_MODEL = False
 SAVE_MODEL = True
-CHECKPOINT = "checkpoints/attempt_2/checkpoint.tar"
+CHECKPOINT = "checkpoints/neural_network/checkpoint.tar"
 EPOCHS = 2000
